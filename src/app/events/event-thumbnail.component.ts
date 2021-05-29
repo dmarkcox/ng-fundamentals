7c8f6a46a33551c43eb8c27166636dedcbe95b91
Don  Mark Cox <dmarkcox@yahoo.com>
2021-05-29 11:20:38 -0700

    ngclass green

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,22 +5,23 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: 'app-event-thumbnail',
   template: `
     <div class="well hoverwell thumbnail">
-      <h2 *ngIf = "event.name">{{event?.name}}</h2>
-      <div *ngIf = "event.date">date: {{event?.date}}</div>
-      <div [ngSwitch]="event?.time">
-      <span *ngSwitch="'8:00 am'">early start</span>
-     <span *ngSwitch="'10:00 am'">late start</span>
-      <span *ngSwitchDefault>normal start</span>
-      </div>
-      <div *ngIf = "event.name">time:{{event?.time}}</div>
-      <div *ngIf = "event.price">price: \${{event?.price}}</div>
-      <div *ngIf = "event?.location">
-        <span>Location: {{event?.location?.address}}</span>
-        <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
-      </div>
-      <div [hidden]= "!event.onlineUrl">
+    <h2 *ngIf = "event.name">{{event?.name}}</h2>
+    <div *ngIf = "event.date">date: {{event?.date}}</div>
+    <div [ngClass]="{green: event?.time == '8:00 am',bold:event?.time=='8:00 am'}"
+    [ngSwitch]="event?.time">Time: {{event?.time}}
+    <span *ngSwitch="'8:00 am'">(early start)</span>
+    <span *ngSwitch="'10:00 am'">(late start)</span>
+    <span *ngSwitchDefault>(normal start)</span>
+    </div>
+    <div *ngIf = "event.name">time:{{event?.time}}</div>
+    <div *ngIf = "event.price">price: \${{event?.price}}</div>
+    <div *ngIf = "event?.location">
+    <span>Location: {{event?.location?.address}}</span>
+    <span>{{event?.location?.city}}, {{event?.location?.country}}</span>
+    </div>
+    <div [hidden]= "!event.onlineUrl">
       online URL: {{event?.onlineUrl}}
-      <div>
+    <div>
     </div>
   `,
   styles: [`
